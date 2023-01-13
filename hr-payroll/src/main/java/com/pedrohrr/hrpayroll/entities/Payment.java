087e36d5dc7f98abef0cc48c0b344a87e5a8286c
Pedro Ramalho <pedro_hrr@hotmail.com>
2023-01-13 19:05:14 -0300

    hr payroll with mock

---
+++ hr-payroll/src/main/java/com/pedrohrr/hrpayroll/entities/Payment.java
@@ -0,0 +1,50 @@
+package com.pedrohrr.hrpayroll.entities;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.io.Serializable;
+
+public class Payment implements Serializable {
+
+    private String name;
+    private Double daylyIncome;
+    private Integer days;
+
+    public Payment () {
+    }
+
+    public Payment(String name, Double daylyIncome, Integer days) {
+        this.name = name;
+        this.daylyIncome = daylyIncome;
+        this.days = days;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDaylyIncome() {
+        return daylyIncome;
+    }
+
+    public void setDaylyIncome(Double daylyIncome) {
+        this.daylyIncome = daylyIncome;
+    }
+
+    public Integer getDays() {
+        return days;
+    }
+
+    public void setDays(Integer days) {
+        this.days = days;
+    }
+
+    public double getTotal(){
+        return days * daylyIncome;
+    }
+
+}
